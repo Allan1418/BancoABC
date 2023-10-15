879c54362e9cf10ec126a1364085bec3b48d54e1
Allan1418 <allannb02@gmail.com>
2023-10-14 18:17:10 -0600

    avance 1 con funcionalidad completa, falta javadoc

--- src/bancoabc/BancoABC.java
+++ src/bancoabc/BancoABC.java
@@ -1,22 +1,29 @@
 
 package bancoabc;
 
+
 import javax.swing.JOptionPane;
 
 
 public class BancoABC {
+    
+    //Variables Globales
+    public static int cantcajas = 0;
+    
+    public static Pila<Cheque> cheques = new Pila<>();
+    
+    public static Cola<Ficha> fichasRegulares = new Cola<>();
+    public static Cola<Ficha> fichasPreferenciales = new Cola<>();
 
     //Metodo principal
     public static void main(String[] args) {
         
         
         //arranque del sistema
-        menu();
+        //menu();
         
         
-        
-        
-        
+        //Pruebas
         
         
     }
@@ -26,7 +33,16 @@ public class BancoABC {
     
     //Metodos menu
     public static void menu(){
-        String[] botones = {"Op1", "Op2", "Op3", "Op4", "SALIR"};
+        
+        String precajas = Comun.regexConfirm("[3-5]", "indique la cantidad de cajas \nPara abrir la sucursal bancaria \n(3-5)", "Solo entre 3 a 5 cajas");
+        if (precajas == null) {
+            return;
+        }
+        
+        cantcajas = Integer.parseInt(precajas);
+        
+        
+        String[] botones = {"Gestionar Cheques Gerencia", "Listar Cajas Activas", "Gestionar Usuarios de Cajas", "Ayuda", "SALIR"};
         int opcion;
         OUTER:
         while (true) {
@@ -36,20 +52,20 @@ public class BancoABC {
                     JOptionPane.DEFAULT_OPTION,
                     JOptionPane.INFORMATION_MESSAGE, 
                     null,
-                    botones, botones[1]);
+                    botones, botones[4]);
 
             switch (opcion) {
                 case 0:
-                    op1();
+                    opChequesGerencia();
                     break;
                 case 1:
-                    //insetar funcion
+                    JOptionPane.showMessageDialog(null, "La sucursal actual del banco ABC tiene " + cantcajas + " cajas activas");
                     break;
                 case 2:
-                    //insetar funcion
+                    opUsuariosCajas();
                     break;
                 case 3:
-                    //insetar funcion
+                    opAyuda();
                     break;
                 default:
                     break OUTER;
@@ -57,14 +73,231 @@ public class BancoABC {
 
         }
     }
+    
+    public static void opChequesGerencia() {
+        
+        String[] botones = {"Ingresar un cheque", "Firmar un cheque", "Listar cheques pendientes", "SALIR"};
+        int opcion;
+        OUTER:
+        while (true) {
+            opcion = JOptionPane.showOptionDialog(null,
+                    "MENU CHEQUES GERENCIA",
+                    "Menu temporal",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.INFORMATION_MESSAGE, 
+                    null,
+                    botones, botones[3]);
 
+            switch (opcion) {
+                case 0:
+                    crearCheque();
+                    break;
+                case 1:
+                    sacarCheque();
+                    break;
+                case 2:
+                    imprCheques();
+                    break;
+                default:
+                    break OUTER;
+            }
 
-    //Metodos de opcion
-    public static void op1() {
+        }
 
+    }
+    
+    public static void opUsuariosCajas() {
+        
+        String[] botones = {"Seleccionar ficha", "Llamar Usuario a cajas", "Mostrar Fichas Pendientes", "SALIR"};
+        int opcion;
+        OUTER:
+        while (true) {
+            opcion = JOptionPane.showOptionDialog(null,
+                    "MENU CHEQUES GERENCIA",
+                    "Menu temporal",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.INFORMATION_MESSAGE, 
+                    null,
+                    botones, botones[3]);
+
+            switch (opcion) {
+                case 0:
+                    crearFicha();
+                    break;
+                case 1:
+                    prepararFicha();
+                    break;
+                case 2:
+                    imprFichas();
+                    break;
+                default:
+                    break OUTER;
+            }
+
+        }
+
+    }
+    
+    public static void opAyuda(){
+        String m = "Banco ABC \n\nIntegrantes de la solucion: \n-Allan Nuñez Brenes  \n-Anyelo  \n-Celeste ";
+        JOptionPane.showMessageDialog(null, m);
     }
 
 
+    //Metodos de opcion
+    public static void crearCheque() {
+        String preNombre, preMonto;
+        
+        preNombre = Comun.regexConfirm(".+", "Ingrese el Titular del cheque", "");
+        if (preNombre == null) {return;}
+        
+        preMonto = Comun.regexConfirm("\\d+([.]\\d+)?", "Ingrese el Monto del Cheque", "Numero Invalido!");
+        if (preMonto == null) {return;}
+        
+        
+        Cheque preCheque = new Cheque(preNombre, Double.parseDouble(preMonto));
+        cheques.push(preCheque);
+        
+        
+    }
+    
+    public static void sacarCheque(){
+        
+        if (cheques.isVacia()) {
+            JOptionPane.showMessageDialog(null, "La pila de cheques esta vacia!");
+            return;
+        }
+        
+        String[] boton = {"Firmar"};
+        JOptionPane.showOptionDialog(null,
+                    cheques.pop().toString(),
+                    "Menu temporal",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.INFORMATION_MESSAGE, 
+                    null,
+                    boton, boton[0]);
+    }
+    
+    public static void imprCheques(){
+        System.out.println("\n---Cheques Pendientes---");
+        cheques.impPila();
+        System.out.println("------------------------");
+    }
+    
+    public static void crearFicha(){
+        
+        boolean tipoCliente;
+        String preCedula, preNombre;
+        
+        String[] botones = {"Preferencial", "Regular", "SALIR"};
+        int opcion = JOptionPane.showOptionDialog(null,
+                    "Seleccione el tipo de Ficha",
+                    "Menu temporal",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.INFORMATION_MESSAGE, 
+                    null,
+                    botones, botones[2]);
+        
+        switch (opcion) {
+            case 0:
+                tipoCliente = true;
+                break;
+            case 1:
+                tipoCliente = false;
+                break;
+            default:
+                return;
+        }
+        
+        preNombre = Comun.regexConfirm(".+", "Ingrese su Nombre", null);
+        if (preNombre == null) {return;}
+        
+        preCedula = Comun.regexConfirm("\\d{9}", "Ingrese su Cedula", "Formato de cedula invalido!");
+        if (preCedula == null) {return;}        
+        
+        Ficha preFicha = new Ficha(preNombre, preCedula, tipoCliente);
+        if (preFicha.isPreferencial()) {
+            fichasPreferenciales.ingresarACola(preFicha);
+        }else{
+            fichasRegulares.ingresarACola(preFicha);
+        }
+        
+        
+    }
+    
+    public static void prepararFicha(){
+        
+        if (fichasRegulares.isVacia() && fichasPreferenciales.isVacia()) {
+            JOptionPane.showMessageDialog(null, "No hay fichas Pendientes!");
+            return;
+        }
+        
+        String regex = "[1-" + String.valueOf(cantcajas) + "]";
+        String preCaja = Comun.regexConfirm(regex, "Ingrese el numero de caja que atiende", "No existe caja!");
+        if (preCaja ==  null) {return;}
+        
+        int cajaActual = Integer.parseInt(preCaja);
+        
+        //caja preferencial
+        if (cajaActual == 1) {
+            if (fichasPreferenciales.isVacia()) {
+                atenderFicha(cajaActual, false);
+                return;
+            }
+            atenderFicha(cajaActual, true);
+            return;
+        }
+        
+        //caja usuarios regulares
+        if (cajaActual == 2) {
+            if (fichasPreferenciales.getSize() > fichasRegulares.getSize()) {
+                atenderFicha(cajaActual, true);
+                return;
+            }
+            atenderFicha(cajaActual, false);
+            return;
+        }
+        
+        //cajas restantes
+        if (fichasRegulares.isVacia()) {
+            atenderFicha(cajaActual, true);
+            return;
+        }
+        
+        atenderFicha(cajaActual, false);
+        
+        
+        
+        
+    }
+    
+    public static void atenderFicha(int numCaja, boolean preferencial){
+        
+        String m;
+        Ficha actual;
+        
+        if (preferencial) {
+            actual = fichasPreferenciales.sacarDeCola();
+        }else{
+            actual = fichasRegulares.sacarDeCola();
+        }
+        
+        //Ficha # <<número de ficha>> con cédula <<cédula de identidad>> pasar a caja <caja asignada>.
+        m = "Ficha #" + actual.getNumero() + " con cédula " + actual.getCedula() + " pasar a caja " + numCaja;
+        
+        JOptionPane.showMessageDialog(null, m);
+    }
+    
+    public static void imprFichas(){
+        System.out.println("\n---Fichas Pendientes---");
+        System.out.println("Fichas Regulares:");
+        fichasRegulares.imprCola();
+        System.out.println("\nFichas Preferenciales:");
+        fichasPreferenciales.imprCola();
+        System.out.println("\n-----------------------");
+    }
+    
+    
     //OtrosMetodos
     
     
---
+++ src/bancoabc/Ficha.java
@@ -0,0 +1,109 @@
+
+package bancoabc;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+public class Ficha {
+    
+    //Variables Globlales
+    private static int contNormal = 1;
+    private static int contPreferencial = 1;
+    
+    //Atributos
+    //Número de ficha, Número de Cédula del cliente, Nombre del cliente, Fecha y hora de llegada(Formato timestamp)
+    private String numero;
+    private String cedula;
+    private String nombre;
+    private Timestamp llegada;
+    private boolean preferencial;
+    
+    
+    //Metodos Get y set
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Timestamp getLlegada() {
+        return llegada;
+    }
+    
+    public String getLlegadaStr(){
+        return new SimpleDateFormat("dd-MM-yyyy HH:mm").format(this.llegada);
+    }
+
+    public void setLlegada(Timestamp llegada) {
+        this.llegada = llegada;
+    }
+    
+    public boolean isPreferencial() {
+        return preferencial;
+    }
+
+    public void setPreferencial(boolean preferencial) {
+        this.preferencial = preferencial;
+    }
+    
+    //Constructores
+    public Ficha() {
+    }
+
+    public Ficha(String nombre, String cedula, boolean preferencial) {
+        
+        if (preferencial) {
+            this.numero = "P" + contPreferencial;
+            contPreferencial++;
+        }else{
+            this.numero = "R" + contNormal;
+            contNormal++;
+        }
+        
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.llegada = new Timestamp(System.currentTimeMillis());
+        this.preferencial = preferencial;
+    }
+    
+    
+    //Otros Metodos
+
+    @Override
+    public String toString() {
+        String verde = "\u001B[32m";
+        String naranja = "\u001B[33m";
+        String reset = "\u001B[0m";
+        String color;
+        
+        if (preferencial) {
+            color = naranja;
+        }else{
+            color = verde;
+        }
+        
+        return color + "Numero[" + numero + "] Nombre[" + nombre + "] Cedula[" + cedula + "] Llegada[" + getLlegadaStr() + "]" + reset;
+    }
+
+    
+    
+    
+}
